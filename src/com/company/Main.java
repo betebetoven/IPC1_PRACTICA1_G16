db5de32b8580a958dfce922abeb74c0a4ab1aab0
Tulio <TulioPirir3@gmail.com>
2021-06-04 13:57:59 -0600

    ruta

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static   Scanner n = new Scanner(System.in);
     public static void main(String[] args) {
 
-	guarda("C:\\Users\\Alberto\\Desktop\\k.txt");
+	guarda("Ruta");
 	convertir(0);
 
     }
